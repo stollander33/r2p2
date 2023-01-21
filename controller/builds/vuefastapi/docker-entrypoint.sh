0772729a06d436b1262db9461b97b3c77a446068
stollander33 <stollander@proton.me>
2023-01-21 08:18:51 +0100

    aññado puerto para la apliacion

--- controller/builds/vuefastapi/docker-entrypoint.sh
+++ controller/builds/vuefastapi/docker-entrypoint.sh
@@ -151,7 +151,7 @@ else
         fi                
         if [[ "$1" == 'api' ]]; then
             echo "Requested api: starting..."
-            poetry run uvicorn app.main:app --host 0.0.0.0 --port 8000            
+            poetry run uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload   
         fi                
         if [[ "$1" == 'worker' ]]; then
             echo "Requested celery: starting..."
